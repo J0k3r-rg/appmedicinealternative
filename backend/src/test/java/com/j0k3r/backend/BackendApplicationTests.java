91409f176d612eabf7550ece4dc4d8e5b88452a5
j0k3r-rg <pdasriogallegos@gmail.com>
2023-12-05 22:17:31 -0300

    init project

---
+++ backend/src/test/java/com/j0k3r/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0k3r.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
